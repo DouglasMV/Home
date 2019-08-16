b61e587940577cb7f613a98e41d9b263222f6254
Douglas Mariano Valero <32845487+DouglasMV@users.noreply.github.com>
2019-08-16 10:35:28 -0300

    Update series.js

--- series.js
+++ series.js
@@ -1,35 +1,23 @@
 let series = [
   {
-      name: "Agents of S.H.I.E.L.D.",
-      link: "https://www.imdb.com/title/tt2364582/episodes?season=6"
-  },
-  {
-      name: "Big Little Lies",
-      link: "https://www.imdb.com/title/tt3920596/episodes?season=2"
-  },
-  {
-      name: "Legion",
-      link: "https://www.imdb.com/title/tt5114356/episodes?season=3"
-  },
-  {
-      name: "Watchmen",
-      link: "https://www.imdb.com/title/tt7049682/episodes?season=1"
-  },
-  {
-      name: "Mr. Robot",
-      link: "https://www.imdb.com/title/tt4158110/episodes?season=4"
+      name: "Modern Family",
+      link: "https://www.imdb.com/title/tt1442437/episodes?season=11"
   },
   {
       name: "Family Guy",
       link: "https://www.imdb.com/title/tt0182576/episodes?season=18"
   },
   {
-      name: "Modern Family",
-      link: "https://www.imdb.com/title/tt1442437/episodes?season=11"
+      name: "Watchmen",
+      link: "https://www.imdb.com/title/tt7049682/episodes?season=1"
   },
   {
       name: "Rick and Morty",
       link: "https://www.imdb.com/title/tt2861424/episodes?season=4"
+  },
+  {
+      name: "Mr. Robot",
+      link: "https://www.imdb.com/title/tt4158110/episodes?season=4"
   },
   {
       name: "Better Call Saul",
@@ -38,6 +26,10 @@ let series = [
   {
       name: "Fargo",
       link: "https://www.imdb.com/title/tt2802850/episodes?season=4"
+  },
+  {
+      name: "Agents of S.H.I.E.L.D.",
+      link: "https://www.imdb.com/title/tt2364582/episodes?season=7"
   }
 ];
 
