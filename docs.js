a34b505462dc2690baac59d58b563450df95cae9
DouglasMV <douglasmv@ymail.com>
2019-10-14 14:06:18 -0300

    All links to docs, add JS tutorial to icons

--- docs.js
+++ docs.js
@@ -1,82 +1,193 @@
 let docs = [
   {
-      name: "Modern JS",
-      link: "https://javascript.info/"
+    name: 'Modern JS',
+    link: 'https://javascript.info/'
   },
   {
-      name: "VS Code",
-      link: "https://code.visualstudio.com/docs/"
+    name: 'VS Code',
+    link: 'https://code.visualstudio.com/docs/'
   },
   {
-      name: "W3 Schools",
-      link: "https://www.w3schools.com/"
+    name: 'W3 Schools',
+    link: 'https://www.w3schools.com/'
   },
   {
-      name: "JS Microsoft",
-      link: "https://msdn.microsoft.com/pt-br/library/yek4tbz0(v=vs.94).aspx"
+    name: 'JS Microsoft',
+    link: 'https://msdn.microsoft.com/pt-br/library/yek4tbz0(v=vs.94).aspx'
   },
   {
-      name: "JQuery",
-      link: "https://jquery.com/"
+    name: 'JQuery',
+    link: 'https://jquery.com/'
   },
   {
-      name: "SASS",
-      link: "http://sass-lang.com/"
+    name: 'SASS',
+    link: 'http://sass-lang.com/'
   },
   {
-      name: "Can I Use",
-      link: "https://caniuse.com/"
+    name: 'Can I Use',
+    link: 'https://caniuse.com/'
   },
   {
-      name: "Bootstrap",
-      link: "http://getbootstrap.com/"
+    name: 'Bootstrap',
+    link: 'http://getbootstrap.com/'
   },
   {
-      name: "Materialize CSS",
-      link: "https://materializecss.com/"
+    name: 'Materialize CSS',
+    link: 'https://materializecss.com/'
   },
   {
-      name: "MDN",
-      link: "https://developer.mozilla.org/pt-BR/"
+    name: 'MDN',
+    link: 'https://developer.mozilla.org/pt-BR/'
   },
   {
-      name: "FCC Guides",
-      link: "https://guide.freecodecamp.org/"
+    name: 'FCC Guides',
+    link: 'https://guide.freecodecamp.org/'
   },
   {
-      name: "Material Design",
-      link: "https://material.io/design/introduction/#"
+    name: 'Material Design',
+    link: 'https://material.io/design/introduction/#'
   },
   {
-      name: "Tutorials Point",
-      link: "https://www.tutorialspoint.com/tutorialslibrary.htm"
+    name: 'Tutorials Point',
+    link: 'https://www.tutorialspoint.com/tutorialslibrary.htm'
   },
   {
-      name: "JS Crockford",
-      link: "http://crockford.com/javascript/"
+    name: 'JS Crockford',
+    link: 'http://crockford.com/javascript/'
   },
   {
-      name: "Built With",
-      link: "https://builtwith.com/"
+    name: 'Built With',
+    link: 'https://builtwith.com/'
   },
   {
-      name: "Heroku",
-      link: "https://devcenter.heroku.com/"
+    name: 'Heroku',
+    link: 'https://devcenter.heroku.com/'
+  },
+  {
+    name: 'React JS',
+    link: 'https://reactjs.org/docs/hello-world.html'
+  },
+  {
+    name: 'React Native',
+    link: 'https://facebook.github.io/react-native/docs/getting-started.html'
+  },
+  {
+    name: 'GraphQL',
+    link: 'https://graphql.org/learn/'
+  },
+  {
+    name: 'Redux',
+    link: 'https://redux.js.org/'
+  },
+  {
+    name: 'Enzyme',
+    link: 'http://airbnb.io/enzyme/'
+  },
+  {
+    name: 'Jest',
+    link: 'https://jestjs.io/docs/en/tutorial-react.html#content'
+  },
+  {
+    name: 'Next JS',
+    link: 'https://nextjs.org/'
+  },
+  {
+    name: 'Parcel JS',
+    link: 'https://parceljs.org/getting_started.html'
+  },
+  {
+    name: 'Create-React-App',
+    link: 'https://github.com/facebook/create-react-app'
+  },
+  {
+    name: 'Babel',
+    link: 'https://babeljs.io/'
+  },
+  {
+    name: 'ESLint',
+    link: 'https://eslint.org/'
+  },
+  {
+    name: 'React Doc-Gen',
+    link: 'https://github.com/reactjs/react-docgen'
+  },
+  {
+    name: 'Story Book',
+    link: 'https://storybook.js.org/'
+  },
+  {
+    name: 'React Refetch',
+    link: 'https://github.com/heroku/react-refetch'
+  },
+  {
+    name: 'Gatsby JS',
+    link: 'https://www.gatsbyjs.org/'
+  },
+  {
+    name: 'Webpack',
+    link: 'https://webpack.js.org/'
+  },
+  {
+    name: 'Node JS',
+    link: 'https://nodejs.org/en/docs/'
+  },
+  {
+    name: 'Express',
+    link: 'http://expressjs.com/'
+  },
+  {
+    name: 'Mongoose',
+    link: 'http://mongoosejs.com/docs/guide.html'
+  },
+  {
+    name: 'Helmet',
+    link: 'https://github.com/helmetjs/helmet'
+  },
+  {
+    name: 'BCrypt',
+    link: 'https://www.npmjs.com/package/bcrypt'
+  },
+  {
+    name: 'Passport JS',
+    link: 'http://www.passportjs.org/'
+  },
+  {
+    name: 'Pug JS',
+    link: 'https://pugjs.org/api/getting-started.html'
+  },
+  {
+    name: 'Node Sec Road Map',
+    link: 'https://nodesecroadmap.fyi/'
+  },
+  {
+    name: 'Rising Stack',
+    link: 'https://blog.risingstack.com/'
+  },
+  {
+    name: 'Node Swat',
+    link: 'https://blog.nodeswat.com/'
+  },
+  {
+    name: 'Gergely Nemeth',
+    link: 'https://nemethgergely.com/'
+  },
+  {
+    name: 'Best Patices',
+    link: 'https://github.com/i0natan/nodebestpractices'
   }
-];
+]
 
-docs.sort((a, b) => a.name > b.name);
+docs.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1))
 
+let ul = document.getElementById('docs')
 
-let ul = document.getElementById("docs");
-
-docs.forEach((item) => {
-  let el = document.createElement('li');
-  let a = document.createElement('a');
-  a.target = "_blank";
-  a.href = item.link;
-  let text = document.createTextNode(item.name);
-  a.appendChild(text);
-  el.appendChild(a);
-  ul.appendChild(el);
-});
+docs.forEach(item => {
+  let el = document.createElement('li')
+  let a = document.createElement('a')
+  a.target = '_blank'
+  a.href = item.link
+  let text = document.createTextNode(item.name)
+  a.append(text)
+  el.append(a)
+  ul.append(el)
+})
--- icons.js
+++ icons.js
@@ -1,119 +1,122 @@
 let icons = [
   {
-    name: "Yahoo",
-    link: "https://br.yahoo.com/?p=us",
-    color: "#6001d2",
-    src: "1.png"
+    name: 'Yahoo',
+    link: 'https://br.yahoo.com/?p=us',
+    color: '#6001d2',
+    src: '1.png'
   },
   {
-    name: "Youtube",
-    link: "https://www.youtube.com/",
-    color: "#ff0000",
-    src: "2.png"
+    name: 'Youtube',
+    link: 'https://www.youtube.com/',
+    color: '#ff0000',
+    src: '2.png'
   },
   {
-    name: "Whatsapp",
-    link: "https://web.whatsapp.com/",
-    color: "#0dc143",
-    src: "3.png"
+    name: 'Whatsapp',
+    link: 'https://web.whatsapp.com/',
+    color: '#0dc143',
+    src: '3.png'
   },
   {
-    name: "FCC",
-    link: "https://guide.freecodecamp.org/",
-    color: "#006400",
-    src: "4.png"
+    name: 'FCC',
+    link: 'https://guide.freecodecamp.org/',
+    color: '#006400',
+    src: '4.png'
   },
   {
-    name: "VS Code",
-    link: "https://www.ingresso.com/santa-barbara-do-oeste/home/cinemas/multiplex-santa-barbara-do-oeste#!#data=20171211",
-    color: "#444444",
-    src: "5.png"
+    name: 'VS Code',
+    link:
+      'https://www.ingresso.com/santa-barbara-do-oeste/home/cinemas/multiplex-santa-barbara-do-oeste#!#data=20171211',
+    color: '#444444',
+    src: '5.png'
   },
   {
-    name: "IMDb",
-    link: "http://www.imdb.com/",
-    color: "#c19d21",
-    src: "6.png"
+    name: 'IMDb',
+    link: 'http://www.imdb.com/',
+    color: '#c19d21',
+    src: '6.png'
   },
   {
-    name: "Fatec",
-    link: "http://www.fatec.edu.br/",
-    color: "#395561",
-    src: "7.png"
+    name: 'Fatec',
+    link: 'http://www.fatec.edu.br/',
+    color: '#395561',
+    src: '7.png'
   },
   {
-    name: "Cisco",
-    link: "https://www.netacad.com/",
-    color: "#049fd9",
-    src: "8.png"
+    name: 'JS Tutorial',
+    link: 'https://javascript.info/',
+    color: '#c8333a',
+    src: '8.png'
   },
   {
-    name: "Codecademy",
-    link: "https://www.codecademy.com/pt/learn",
-    color: "#284054",
-    src: "9.png"
+    name: 'Codecademy',
+    link: 'https://www.codecademy.com/',
+    color: '#284054',
+    src: '9.png'
   },
   {
-    name: "Udemy",
-    link: "https://www.udemy.com/",
-    color: "#ed5353",
-    src: "10.png"
+    name: 'Udemy',
+    link: 'https://www.udemy.com/',
+    color: '#ed5353',
+    src: '10.png'
   },
   {
-    name: "Github",
-    link: "https://github.com/",
-    color: "#000000",
-    src: "11.png"
+    name: 'Github',
+    link: 'https://github.com/',
+    color: '#000000',
+    src: '11.png'
   },
   {
-    name: "W3 Schools",
-    link: "https://www.w3schools.com/",
-    color: "#87b515",
-    src: "12.png"
+    name: 'W3 Schools',
+    link: 'https://www.w3schools.com/',
+    color: '#87b515',
+    src: '12.png'
   },
   {
-    name: "Explosm",
-    link: "http://explosm.net/",
-    color: "#2f264d",
-    src: "13.png"
+    name: 'Explosm',
+    link: 'http://explosm.net/',
+    color: '#2f264d',
+    src: '13.png'
   },
   {
-    name: "Tutorials Point",
-    link: "https://www.tutorialspoint.com/tutorialslibrary.htm",
-    color: "#34a360",
-    src: "14.png"
+    name: 'Tutorials Point',
+    link: 'https://www.tutorialspoint.com/tutorialslibrary.htm',
+    color: '#34a360',
+    src: '14.png'
   },
   {
-    name: "Google Drive",
-    link: "https://drive.google.com/drive/my-drive",
-    color: "#d0d0d0",
-    src: "15.png"
+    name: 'Google Drive',
+    link: 'https://drive.google.com/drive/my-drive',
+    color: '#d0d0d0',
+    src: '15.png'
   },
   {
-    name: "MDN",
-    link: "https://developer.mozilla.org/en-US/",
-    color: "#81d1f1",
-    src: "16.png"
+    name: 'MDN',
+    link: 'https://developer.mozilla.org/en-US/',
+    color: '#81d1f1',
+    src: '16.png'
   }
 ]
 
-let main_div = document.getElementById("icons-main");
+let main_div = document.getElementById('icons-main')
 
-icons.forEach((item) => {
-  let el = document.createElement("div");
-  let a = document.createElement("a");
-  let circle = document.createElement("div");
-  let img = document.createElement("img");
+icons.forEach(item => {
+  let el = document.createElement('div')
+  let a = document.createElement('a')
+  let circle = document.createElement('div')
+  let img = document.createElement('img')
 
-  el.className = "grid-item";
-  a.target = "_blank";
-  a.href = item.link;
-  circle.className = "circle";
-  circle.style.backgroundColor = item.color;
-  img.src = "./imgs/" + item.src;
-  
-  circle.appendChild(img);
-  a.appendChild(circle);
-  el.appendChild(a);
-  main_div.appendChild(el);
-});
+  el.className = 'grid-item'
+  a.target = '_blank'
+  a.href = item.link
+  circle.className = 'circle'
+  circle.style.backgroundColor = item.color
+  img.src = './imgs/' + item.src
+  img.alt = item.name
+  img.title = item.name
+
+  circle.append(img)
+  a.append(circle)
+  el.append(a)
+  main_div.append(el)
+})
